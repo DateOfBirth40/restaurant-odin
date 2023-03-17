9b3f73688d5ea582b752b831a642258ea0942391
Derek O'Brien <derekobrien40@gmail.com>
2023-03-17 14:45:25 -0400

    Create function to add to DOM; not functional yet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
-content = document.querySelector('#content');
-headerTitle = document.createElement('div');
+const content = document.querySelector('#content');
+const headerTitle = document.createElement('div');
 headerTitle.textContent = 'Flanigan\'s';
 content.appendChild(headerTitle);
 
@@ -14,37 +14,69 @@ content.appendChild(about);
 // make each li item a flex element and add the title, description, and price
 // find out how to add all objects to an array
 
-function MenuItem(name, description, price) {
+function MenuItem(type, name, description, price) {
+  this.type = type;
   this.name = name;
   this.description = description;
   this.price = price;
 };
 
 // STARTERS
-const ribRolls = new MenuItem('Joe\'s Rockin\' Rib Rolls', 'Homemade, deep-fried egg rolls stuffed with meat from our famous baby back ribs, pulled pork, cheddar cheese, onions, and Flanigan\'s BBQ sauce.', '11.99');
-const tumbleweedOnions = new MenuItem('Tumbleweed Onions', 'Thinly sliced jumbo onions, lightly battered and fried, with a side of honey mustard.', '6.99');
-const friedShrimp = new MenuItem('Firecracker Shrimp', 'A dozen plump, deep-fried shrimp, tossed in a slightly spicy cream sauce over a bed of iceberg lettuce, topped with pico di gallo.', '11.99');
+const ribRolls = new MenuItem('starter', 'Joe\'s Rockin\' Rib Rolls', 'Homemade, deep-fried egg rolls stuffed with meat from our famous baby back ribs, pulled pork, cheddar cheese, onions, and Flanigan\'s BBQ sauce.', '11.99');
+const tumbleOnions = new MenuItem('starter', 'Tumbleweed Onions', 'Thinly sliced jumbo onions, lightly battered and fried, with a side of honey mustard.', '6.99');
+const friedShrimp = new MenuItem('starter', 'Firecracker Shrimp', 'A dozen plump, deep-fried shrimp, tossed in a slightly spicy cream sauce over a bed of iceberg lettuce, topped with pico di gallo.', '11.99');
 
 // TEN OUNCE BURGERS
-const texasBurg = new MenuItem('Texas Burger', 'Crispy bacon, crumbled bleu cheese, a stack of Tumbleweed Onions, all drizzled with our homemade wing sauce.', '12.49');
-const caribbeanBurg = new MenuItem('Caribbean Burger', 'Flanigan\'s award-winning BBQ sauce, melted cheddar cheese, and two strips of bacon.', '12.49');
+const texasBurg = new MenuItem('burger', 'Texas Burger', 'Crispy bacon, crumbled bleu cheese, a stack of Tumbleweed Onions, all drizzled with our homemade wing sauce.', '12.49');
+const caribBurg = new MenuItem('burger', 'Caribbean Burger', 'Flanigan\'s award-winning BBQ sauce, melted cheddar cheese, and two strips of bacon.', '12.49');
 
 // WAY TOO BIG SANDWICHES
-const cheeseSteak = new MenuItem('Philadelphia Cheese Steak', 'Sliced beef sautéed with onions, and mushrooms with melted Swiss American cheese on a hoagie roll.', '11.99');
-const chipotleChicken = new MenuItem('Chipotle Chicken Stack Sandwich', 'Grilled chicken breast with melted Swiss cheese, crisp bacon, chipotle sauce, and a pile of Tumbleweed Onions.', '10.99');
-const dolphinSand = new MenuItem('Dolphin Sandwich', 'Dolphin (aka Mahi-Mahi) with lettuce and tomato on a Kaiser roll. Tartar sauce available upon request. Blackened, Fried, or Grilled.', 'Market Price');
+const cheeseStk = new MenuItem('sandwich', 'Philadelphia Cheese Steak', 'Sliced beef sautéed with onions, and mushrooms with melted Swiss American cheese on a hoagie roll.', '11.99');
+const chipotChicken = new MenuItem('sandwich', 'Chipotle Chicken Stack Sandwich', 'Grilled chicken breast with melted Swiss cheese, crisp bacon, chipotle sauce, and a pile of Tumbleweed Onions.', '10.99');
+const dolphinSand = new MenuItem('sandwich', 'Dolphin Sandwich', 'Dolphin (aka Mahi-Mahi) with lettuce and tomato on a Kaiser roll. Tartar sauce available upon request. Blackened, Fried, or Grilled.', 'Market Price');
 
 // SEAFOOD
-const grilledSalmon = new MenuItem('Grilled Salmon', 'A chargrilled ten-ounce filet of Atlantic Salmon. Your pick - Lemon Garlic, Toasted Sesame Glaze, or Blackened.', '18.99');
-const surfTurf = new MenuItem('Surf & Turf', 'An eight-ounce USDA Choice N.Y. Strip Steak, paired with 1/2 dozen plump fried shrimp.', '19.99');
-const lemonTilapia = new MenuItem('Lemon Pepper Tilapia', 'Tender, flaky Tilapia sautéed in our creamy, homemade Lemon Pepper sauce.', '16.99');
+const grilledSalmon = new MenuItem('seafood', 'Grilled Salmon', 'A chargrilled ten-ounce filet of Atlantic Salmon. Your pick - Lemon Garlic, Toasted Sesame Glaze, or Blackened.', '18.99');
+const surfTurf = new MenuItem('seafood', 'Surf & Turf', 'An eight-ounce USDA Choice N.Y. Strip Steak, paired with 1/2 dozen plump fried shrimp.', '19.99');
+const lemonTilapia = new MenuItem('seafood', 'Lemon Pepper Tilapia', 'Tender, flaky Tilapia sautéed in our creamy, homemade Lemon Pepper sauce.', '16.99');
 
 // SIDES
-const bakedPotato = new MenuItem('Baked Potato', 'Idaho\'s best, served piping hot with sour cream and butter piled on at your request.', '3.99');
-const frenchFries = new MenuItem('French Fries', 'A Flanigan\'s signature, 3/4 of a pound of perfectly seasoned curly fries.', '6.99');
-const sweetPotato = new MenuItem('Sweet Potato Fries', 'A generous amount of the ultimate sweet potato fry, deep-fried, and dusted with cinnamon sugar. Sub side with any sandwich, burger, or entree.........Add $1.99', '7.99');
+const bakedPotato = new MenuItem('side', 'Baked Potato', 'Idaho\'s best, served piping hot with sour cream and butter piled on at your request.', '3.99');
+const frenchFries = new MenuItem('side', 'French Fries', 'A Flanigan\'s signature, 3/4 of a pound of perfectly seasoned curly fries.', '6.99');
+const sweetPotato = new MenuItem('side', 'Sweet Potato Fries', 'A generous amount of the ultimate sweet potato fry, deep-fried, and dusted with cinnamon sugar. Sub side with any sandwich, burger, or entree.........Add $1.99', '7.99');
+
+menuArr = [ribRolls, tumbleOnions, friedShrimp, texasBurg, caribBurg, cheeseStk, chipotChicken, dolphinSand, grilledSalmon, surfTurf, lemonTilapia, bakedPotato, frenchFries, sweetPotato];
+
+// Create a loop that goes through each element in the array and adds each property of the object to its
+// respective p element in the list div
+// for ex., listItemTitle.textContent = this.name would add 'Grilled Salmon' to the list div
 
 menu = document.createElement('div');
 menuList = document.createElement('ul');
+content.appendChild(menu);
+menu.appendChild(menuList);
 
-// function createMenu()
+function createMenuListItem() {
+  // Add classes to these elements eventually for CSS styling
+  const menuListItem = document.createElement('li');
+  const menuListItemDiv = document.createElement('div');
+  const menuListItemName = document.createElement('p');
+  const menuListItemDesc = document.createElement('p');
+  const menuListItemPrice = document.createElement('p');
+
+  menuList.appendChild(menuListItem);
+  menuListItem.appendChild(menuListItemDiv);
+  menuListItemDiv.appendChild(menuListItemName);
+  menuListItemDiv.appendChild(menuListItemDesc);
+  menuListItemDiv.appendChild(menuListItemPrice);
+}
+
+// menuArr.forEach(function(parameter) {
+
+// });
+
+// Can use this to filter by type
+// Maybe create a function that accepts item type as a parameter and creates an array of just that type
+const sideItems = menuArr.filter((item) => item.type === 'side');
+console.log(sideItems);
+console.log(menuArr);
