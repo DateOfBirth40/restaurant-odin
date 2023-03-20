f001f87670111812695bdad49694feec60d9a40e
Derek O'Brien <derekobrien40@gmail.com>
2023-03-20 19:15:05 -0400

    Functions export to index.js properly; 'hours' and 'img' objects do not work

--- src/locations.js
+++ src/locations.js
@@ -1,7 +1,5 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable max-len */
-// Create an object for the different locations
-// Properties: name, address, hours
 
 function Location(name, address, phone, hours, img) {
   this.name = name;
@@ -17,4 +15,52 @@ const pinecrest = new Location('Pinecrest', '11415 S Dixie Highway, Miami, FL 33
 const westchester = new Location('Westchester', '9857 SW 40th Street, Miami, FL 33165', '(305) 207-7427', {open: '11', close: '1:30'}, './westchester.png');
 const coconutGrove = new Location('Coconut Grove', '2721 Bird Avenue, Miami, FL 33133', '(305) 446-1114', {open: '11', close: '4:30'}, './coconut-grove.png');
 
-locArr = [doral, kendall, pinecrest, westchester, coconutGrove];
+const locArr = [doral, kendall, pinecrest, westchester, coconutGrove];
+
+const locationDiv = document.createElement('div');
+const locationList = document.createElement('ul');
+content.appendChild(locationDiv);
+locationDiv.appendChild(locationList);
+
+function createLocationListItem() {
+  const locationListItem = document.createElement('li');
+  locationListItem.classList.add('loc-list-item');
+  const locationListItemDiv = document.createElement('div');
+  locationListItemDiv.classList.add('loc-list-item-div');
+  const locationListItemName = document.createElement('h3');
+  locationListItemName.classList.add('loc-list-item-name');
+  const locationListItemAddress = document.createElement('p');
+  locationListItemAddress.classList.add('loc-list-item-address');
+  const locationListItemPhone = document.createElement('p');
+  locationListItemPhone.classList.add('loc-list-item-phone');
+  //   Find a way to access the object within the 'hours' property
+  const locationListItemHours = document.createElement('p');
+  locationListItemHours.classList.add('loc-list-item-hours');
+  const locationListItemImg = document.createElement('img');
+  locationListItemImg.classList.add('loc-list-item-img');
+
+  locationList.appendChild(locationListItem);
+  locationListItem.appendChild(locationListItemDiv);
+  locationListItemDiv.appendChild(locationListItemName);
+  locationListItemDiv.appendChild(locationListItemAddress);
+  locationListItemDiv.appendChild(locationListItemPhone);
+  locationListItemDiv.appendChild(locationListItemHours);
+  locationListItemDiv.appendChild(locationListItemImg);
+  return [locationListItemName, locationListItemAddress, locationListItemPhone, locationListItemHours, locationListItemImg];
+}
+
+// Change this to a for loop
+function addLocationText(arr) {
+  arr[0].textContent = this.name;
+  arr[1].textContent = this.address;
+  arr[2].textContent = this.phone;
+  arr[3].textContent = this.hours;
+//   arr[4].____ = this.img;
+}
+
+export {
+  createLocationListItem,
+  addLocationText,
+  locArr,
+  locationList,
+};
