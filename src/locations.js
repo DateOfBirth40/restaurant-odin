22b5cec4f3968dc42d471b0be04cbee2c8bd8eee
Derek O'Brien <derekobrien40@gmail.com>
2023-03-27 14:26:00 -0400

    Simplify DOM; add new city prop. to object, sort locArr

--- src/locations.js
+++ src/locations.js
@@ -6,64 +6,60 @@ import kendallPic from './kendall.png';
 import pinecrestPic from './pinecrest.png';
 import westchesterPic from './westchester.png';
 
-function Location(name, address, phone, hours, img) {
+function Location(name, address, city, phone, hours, img) {
   this.name = name;
   this.address = address;
+  this.city = city;
   this.phone = phone;
   this.hours = hours;
   this.img = img;
 };
 
 const locArr = [];
-function createLocation(name, address, phone, hours, img) {
-  const location = new Location(name, address, phone, hours, img);
+function createLocation(name, address, city, phone, hours, img) {
+  const location = new Location(name, address, city, phone, hours, img);
   locArr.push(location);
   return location;
 }
+const coconutGrove = createLocation('Coconut Grove', '2721 Bird Avenue', 'Miami, FL 33133', '(305) 446-1114', {open: '11 AM', close: '4:30 AM'}, coconutGrovePic);
+const doral = createLocation('Doral', '8695 NW 12th Street', 'Miami, FL 33126', '(786) 845-9366', {open: '11 AM', close: '1 AM'}, doralPic);
+const kendall = createLocation('Kendall', '12790 SW 88th Street', 'Miami, FL 33186', '(305) 380-0521', {open: '11 AM', close: '1 AM'}, kendallPic);
+const pinecrest = createLocation('Pinecrest', '11415 S Dixie Highway', 'Miami, FL 33156', '(305) 378-4000', {open: '11 AM', close: '1 AM'}, pinecrestPic);
+const westchester = createLocation('Westchester', '9857 SW 40th Street', 'Miami, FL 33165', '(305) 207-7427', {open: '11 AM', close: '1:30 AM'}, westchesterPic);
 
-const coconutGrove = createLocation('Coconut Grove', '2721 Bird Avenue, Miami, FL 33133', '(305) 446-1114', {open: '11', close: '4:30'}, coconutGrovePic);
-const doral = createLocation('Doral', '8695 NW 12th Street, Miami, FL 33126', '(786) 845-9366', {open: '11', close: '1'}, doralPic);
-const kendall = createLocation('Kendall', '12790 SW 88th Street, Miami, FL 33186', '(305) 380-0521', {open: '11', close: '1'}, kendallPic);
-const pinecrest = createLocation('Pinecrest', '11415 S Dixie Highway, Miami, FL 33156', '(305) 378-4000', {open: '11', close: '1'}, pinecrestPic);
-const westchester = createLocation('Westchester', '9857 SW 40th Street, Miami, FL 33165', '(305) 207-7427', {open: '11', close: '1:30'}, westchesterPic);
+locArr.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+console.log(locArr);
 
 const locationDiv = document.createElement('div');
-const locationList = document.createElement('ul');
+locationDiv.classList.add('location-div');
 content.appendChild(locationDiv);
-locationDiv.appendChild(locationList);
 
 // Add get directions button with a link to the address below each location
 function createLocationListItem() {
-  const locationListItem = document.createElement('li');
-  locationListItem.classList.add('loc-list-item');
-  const locationListItemDiv = document.createElement('div');
-  locationListItemDiv.classList.add('loc-list-item-div');
+  const locationListItem = document.createElement('div');
   const locationListItemName = document.createElement('h3');
-  locationListItemName.classList.add('loc-list-item-name');
+  locationListItemName.classList.add('item-name');
   const locationListItemAddress = document.createElement('p');
-  locationListItemAddress.classList.add('loc-list-item-address');
+  const locationListItemCity = document.createElement('p');
   const locationListItemPhone = document.createElement('p');
-  locationListItemPhone.classList.add('loc-list-item-phone');
   const locationListItemHours = document.createElement('p');
-  locationListItemHours.classList.add('loc-list-item-hours');
   const locationListItemImg = document.createElement('img');
-  locationListItemImg.classList.add('loc-list-item-img');
 
-  locationList.appendChild(locationListItem);
-  locationListItem.appendChild(locationListItemDiv);
-  locationListItemDiv.appendChild(locationListItemName);
-  locationListItemDiv.appendChild(locationListItemAddress);
-  locationListItemDiv.appendChild(locationListItemPhone);
-  locationListItemDiv.appendChild(locationListItemHours);
-  locationListItemDiv.appendChild(locationListItemImg);
-  return [locationListItemName, locationListItemAddress, locationListItemPhone, locationListItemHours, locationListItemImg];
+  locationDiv.appendChild(locationListItem);
+  locationListItem.appendChild(locationListItemName);
+  locationListItem.appendChild(locationListItemAddress);
+  locationListItem.appendChild(locationListItemCity);
+  locationListItem.appendChild(locationListItemPhone);
+  locationListItem.appendChild(locationListItemHours);
+  locationListItem.appendChild(locationListItemImg);
+  return [locationListItemName, locationListItemAddress, locationListItemCity, locationListItemPhone, locationListItemHours, locationListItemImg];
 }
 
 function addLocationText(arr) {
-  const properties = [this.name, this.address, this.phone, this.hours, this.img];
+  const properties = [this.name, this.address, this.city, this.phone, this.hours, this.img];
   for (let i = 0; i < arr.length; i++) {
     if (properties[i] === this.hours) {
-      arr[i].textContent = `Every Day: ${this.hours['open']} AM - ${this.hours['close']} AM`;
+      arr[i].textContent = `Every Day: ${this.hours['open']} - ${this.hours['close']}`;
     } else if (properties[i] === this.img) {
       arr[i].src = properties[i];
     } else {
@@ -76,5 +72,5 @@ export {
   createLocationListItem,
   addLocationText,
   locArr,
-  locationList,
+  locationDiv,
 };
